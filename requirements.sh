91e01fe9fca390c1f85c47cebdff9f916fafb2be
José Ilberto <ilbertofjunior@gmail.com>
2019-03-20 15:24:21 +0100

    Added requirements files

---
+++ requirements.sh
@@ -0,0 +1,9 @@
+sudo add-apt-repository ppa:deadsnakes/ppa -y
+sudo apt-get update
+sudo apt-get install python3.6  python3.6-venv -y
+python3.6 -m pip install
+pip3 install virtualenv
+python3 -m venv ./venv
+pyenv-3.6 ./venv
+source ./venv/bin/activate
+pip install -r requirements_python.txt
