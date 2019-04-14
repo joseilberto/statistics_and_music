2d29409cca64c6613eaf2ac3113fe2e348f2c6c5
José <ilbertofjunior@gmail.com>
2019-04-14 14:11:47 +0200

    [transform] Files are saved in a different folder

--- transform/processors.py
+++ transform/processors.py
@@ -4,6 +4,11 @@ import pandas as pd
 
 
 class Properties_Calculator:
+    @property
+    def redo_sigma_entropy(self):
+        return self.kwargs.get("redo_sigma_entropy", False)
+
+
     def calculate_sigma_and_entropy(self, file, *args, **kwargs):
         def calculate_entropy(probabilities):
             entropy = 0
@@ -31,5 +36,26 @@ class Properties_Calculator:
 
     def create_sigma_entropy_files(self, files, *args, **kwargs):
         for file in files:
-            rel_data = self.calculate_sigma_and_entropy(file)
-            
+            base_folder = os.path.dirname(file) + "/sigma_entropy/"
+            file_base = os.path.basename(file).replace("_transformed",
+                                                                "{}")
+            os.makedirs(base_folder, exist_ok = True)
+            output_base = (base_folder + file_base).format
+            output_file = output_base("_statistics")
+            if not os.path.isfile(output_file) or self.redo_sigma_entropy:
+                data = self.calculate_sigma_and_entropy(file)
+                self.save_sigma_entropy_data(data, output_file)
+                self.save_top10(data, output_base("_top10_frequency"), "k")
+                self.save_top10(data, output_base("_top10_entropy"), "entropy")
+                self.save_top10(data, output_base("_top10_sigma"), "sigma")
+
+
+    def save_sigma_entropy_data(self, data, output_file, *args, **kwargs):
+        data.sort_values(by = ["sigma"], inplace = True)
+        data.to_csv(output_file, index = False)
+
+
+    def save_top10(self, data, output_file, column = "sigma", top = 10, *args,
+                                                                    **kwargs):
+        data.sort_values(by = [column], inplace = True)
+        data.head(top).to_csv(output_file, index = False)
